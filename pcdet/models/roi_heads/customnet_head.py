069fda70db3f47b7a9c3a63df736b83309a46803
YS-Kuang <83287399+YS-Kuang@users.noreply.github.com>
2023-05-22 16:05:12 +0800

    Update customnet_head.py

--- pcdet/models/roi_heads/customnet_head.py
+++ pcdet/models/roi_heads/customnet_head.py
@@ -14,7 +14,7 @@ class CustomNetHead(RoIHeadTemplate):
     def __init__(self, model_cfg, input_channels, num_class, code_size, **kwargs):
         super().__init__(num_class=num_class, model_cfg=model_cfg)
 
-        self.AoA_mat = np.load(r'/content/CalibrationTable.npy', allow_pickle=True).item()
+        self.AoA_mat = np.load(r'/content/sample_data/Valeo/CalibrationTable.npy', allow_pickle=True).item()
         self.model_cfg = model_cfg
 
         self.radar_channels = [512]
@@ -85,7 +85,7 @@ class CustomNetHead(RoIHeadTemplate):
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         batch_size = radar_features.shape[0]
         doppler_indexes = np.array(self.AoA_mat['doppler_indexes'])
-        window = torch.from_numpy(self.AoA_mat['H'][0]).to(device)
+        window = torch.from_numpy(self.AoA_mat['H'][0]).float().to(device)
         CalibMat = torch.from_numpy(self.AoA_mat['Signal'][...,5]).to(device)
         MIMO_Spectrum = radar_features[:,:, doppler_indexes, :].reshape(batch_size, radar_features.shape[1] * radar_features.shape[2], -1)
         MIMO_Spectrum = torch.multiply(MIMO_Spectrum, window)
@@ -97,7 +97,7 @@ class CustomNetHead(RoIHeadTemplate):
 
     def get_radar_features(self, radar_cubes, masks):
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        r_axis_m = torch.from_numpy(self.AoA_mat['r_axis_m']).to(device)
+        r_axis_m = torch.from_numpy(self.AoA_mat['r_axis']).to(device)
         sin_azi_axis = torch.from_numpy(self.AoA_mat['sin_azi_axis']).to(device)
         doppler_features = []
         for batch_idx in range(radar_cubes.shape[0]):
@@ -133,7 +133,7 @@ class CustomNetHead(RoIHeadTemplate):
         doppler_features = self.get_radar_features(radar_cubes, batch_dict['roi_masks']) # (B, N, 9, 256)
         
         # radar modules
-        doppler_features = batch_dict['roi_radar_features'].view(-1, batch_dict['roi_radar_features'].shape[2], 256) # (B * N, 9, 256)
+        doppler_features = doppler_features.view(-1, doppler_features.shape[2], 256) # (B * N, 9, 256)
         doppler_features = doppler_features.reshape(-1, 3, 3, 256)
         doppler_features = doppler_features.permute(0, 3, 1, 2).contiguous()
         features = self.radar_layer(doppler_features) # (B*N, 512, 1, 1)
